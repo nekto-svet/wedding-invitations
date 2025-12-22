45c92d549223f61ae435c15eb5488769a377b28a
Sveta Livshits <nekto.nap@gmail.com>
2025-12-22 21:32:48 +0200

    hack page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import InvitationPage from './InvitationPage'
 import FirstInvitation from './FirstInvitation'
+import HackPage from './HackPage'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/second/:lang/:username" element={<InvitationPage />} />
+        <Route path="/count/:lang/:username" element={<InvitationPage />} />
         <Route path="/first/:lang/:username" element={<FirstInvitation />} />
+        <Route path="/second/:lang/:username" element={<HackPage />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/HackPage.jsx
@@ -0,0 +1,18 @@
+function HackPage() {
+  return (
+    <div style={{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      minHeight: '100vh',
+      fontSize: '24px',
+      textAlign: 'center',
+      padding: '20px'
+    }}>
+      You smart ass, trying to hack the system
+    </div>
+  )
+}
+
+export default HackPage
+
